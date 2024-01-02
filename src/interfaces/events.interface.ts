dcd6e6fb0c390881cc49669ad493f466e17b8703
Ibukun Alesinloye <hiibeekayvibe@gmail.com>
2024-01-02 10:08:55 -0800

    Initialize Implement-Social-Link-Retrival

---
+++ src/interfaces/events.interface.ts
@@ -0,0 +1,18 @@
+// Interface for payload to create events
+interface createEventsInterface {
+  title: string;
+  description?: string;
+  startDate: Date;
+  endDate: Date;
+  time: string;
+  location: string;
+  capacity: number;
+  entranceFee?: number;
+  eventType: string;
+  organizerID: string;
+  categoryID?: string;
+}
+
+interface editEventsInterface extends createEventsInterface {}
+
+export { createEventsInterface, editEventsInterface };
